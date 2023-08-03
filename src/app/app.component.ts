db507f2c44a1678aa6924f079d7bfc11407fc694
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-08-03 14:06:51 +0800

    add online and offline evenlisteners to update accordingly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'csf-pwa';
+
+  isOnline: boolean;
+
+  constructor() {
+    this.isOnline = false;
+  }
+
+  ngOnInit(): void {
+    this.updateOnlineStatus();
+  }
+  
+  private updateOnlineStatus(): void {
+    this.isOnline = window.navigator.onLine;
+
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
 }
